a570a93dfdc07957c58ffd3c9c141cc0048fea04
Kevin Martensson <kevinmartensson@gmail.com>
2017-05-22 18:40:18 +0200

    Add `--mime` flag

--- cli.js
+++ cli.js
@@ -7,17 +7,26 @@ const cli = meow(`
 	Usage
 	  $ ext-name <input>
 
+	Options
+	  --mime Get extension and MIME type from a MIME type
+
 	Example
 	  $ ext-name file.tar
 	  tar
 	  application/x-tar
-`);
+
+	  $ ext-name --mime application/x-tar
+	  tar
+	  application/x-tar
+`, {
+	boolean: ['mime']
+});
 
 if (cli.input.length === 0) {
 	console.error('Specify a file');
 	process.exit(1);
 }
 
-const ext = extName(cli.input[0]);
+const ext = cli.flags.mime ? extName.mime(cli.input[0]) : extName(cli.input[0]);
 
 console.log(`${ext.ext}\n${ext.mime}`);
--- test.js
+++ test.js
@@ -6,10 +6,7 @@ test('dislay ext and mime type', async t => {
 	t.is(await execa.stdout('./cli.js', ['foobar.gif']), 'gif\nimage/gif');
 });
 
-test('show help screen', async t => {
-	t.regex(await execa.stdout('./cli.js', ['--help']), /Get the file extension and MIME type from a file/);
-});
-
-test('show version', async t => {
-	t.is(await execa.stdout('./cli.js', ['--version']), require('./package').version);
+test('dislay ext and mime type using mime type', async t => {
+	t.is(await execa.stdout('./cli.js', ['--mime', 'application/x-tar']), 'tar\napplication/x-tar');
+	t.is(await execa.stdout('./cli.js', ['--mime', 'image/gif']), 'gif\nimage/gif');
 });
